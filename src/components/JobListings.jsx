1dff0b21ae7c2e666162e5729b8082ec3a9ef562
IbrahemMohammad <Ibrahem.mohammad.acm.999@gmail.com>
2024-07-25 16:55:48 +0400

    add Spinner

--- src/components/JobListings.jsx
+++ src/components/JobListings.jsx
@@ -1,5 +1,6 @@
 import JobListing from './JobListing'
 import { userState, useEffect, useState } from 'react'
+import Spinner from './Spinner'
 
 const JobListings = ({ isHome = false }) => {
 
@@ -8,11 +9,12 @@ const JobListings = ({ isHome = false }) => {
 
     useEffect(() => {
         const fetchJobs = async () => {
+            const apiUrl = isHome ? '/api/jobs?_limit=3' : '/api/jobs'
             try {
-                const res = await fetch('http://localhost:5000/jobs')
-                const data = res.json()
+                // await new Promise(resolve => setTimeout(resolve, 2000));
+                const res = await fetch(apiUrl)
+                const data = await res.json()
                 setJobs(data)
-                console.log('jobs  ', jobs)
             } catch (error) {
                 console.log('Error fetching data ', error)
             } finally {
@@ -23,6 +25,8 @@ const JobListings = ({ isHome = false }) => {
         fetchJobs();
     }, [])
 
+    console.log('jobs  ', jobs)
+
     return (
         <>
             <section className="bg-blue-50 px-4 py-10">
@@ -30,11 +34,15 @@ const JobListings = ({ isHome = false }) => {
                     <h2 className="text-3xl font-bold text-indigo-500 mb-6 text-center">
                         {isHome ? 'Recent Jobs' : 'Browse Jobs'}
                     </h2>
-                    <div className="grid grid-cols-1 md:grid-cols-3 gap-6">
-                        {jobs.map((job) => (
-                            <JobListing key={job.id} job={job} />
-                        ))}
-                    </div>
+                    {loading ? (
+                        <Spinner loading={loading} />
+                    ) : (
+                        <div className="grid grid-cols-1 md:grid-cols-3 gap-6">
+                            {jobs.map((job) => (
+                                <JobListing key={job.id} job={job} />
+                            ))}
+                        </div>
+                    )}
                 </div>
             </section>
         </>
